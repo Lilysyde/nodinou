773b2cdfd923b41769012ba47164626cee7dd726
Emilie Barlet <emiliegrimoud@yahoo.fr>
2019-03-20 12:20:44 +0100

    node

---
+++ atelierNode.js
@@ -0,0 +1,6 @@
+const chalk = require('chalk');
+ 
+console.log(chalk.blue('remy'));
+console.log(chalk.magentaBright('valentin'));
+console.log(chalk.red('omar'));
+console.log(chalk.cyan('fred'));
